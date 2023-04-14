fb6f0bf1085ff8922458f8fbdc5b7641b407e19d
Adriana Buitrago <buitragoadr@gmail.com>
2023-04-14 17:07:04 +0200

    Ejercicio terminado

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,33 @@
+import { Link } from 'react-router-dom'
 import trashCan from '../assets/images/trash-can.svg'
+import checkbox from '../assets/images/checkbox-checked.svg'
+import checkboxNotChecked from '../assets/images/checkbox-not-checked.svg'
 import styles from './Todo.module.css'
 
-function Todo({ todo }) {
-  return <li className={styles.todo_item}>
-    <span className={styles.content}>{todo.content}</span> 
-    <button className={styles.trashButton}>
-      <img src={trashCan} alt="Borrar" className={styles.trashCan} />
-    </button>
-  </li>
+function Todo({ todo, deleteTodo, checkButton }) {
+
+    const deleteButtonHandler = () => {
+        deleteTodo(todo.id)
+    }
+
+    const checkButtonHandler = () => {
+        checkButton({ id: todo.id, done: !todo.done })
+    }
+
+    const icon = todo.done ? checkbox : checkboxNotChecked
+
+    return <li className={styles.todo_item}>
+        <span className={styles.content}>
+            <Link className={styles.content} onClick={checkButtonHandler}> 
+                <img src={icon} className={styles.icon} alt=""/>
+            </Link>
+            {todo.content}
+            </span>
+        <button className={styles.trashButton} onClick={deleteButtonHandler}>
+            <img src={trashCan} alt="Borrar" className={styles.trashCan} />
+        </button>
+    </li>
+    
 }
 
 export default Todo
